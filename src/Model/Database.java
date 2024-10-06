a63aa4794906bc0985cfd36f7f04fbb714b4df03
not-utkarsh201 <work.utkarsh201@gmail.com>
2024-10-06 23:14:49 +0530

    uploaded

---
+++ src/Model/Database.java
@@ -0,0 +1,31 @@
+package Model;
+
+import View.Alert;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+	
+	private String user = "root";
+	private String pass = "";
+	private String url = "jdbc:mysql://localhost:3306/socialmedia";
+	private Statement statement;
+	
+	public Database() {
+		try {
+			Connection connection = DriverManager.getConnection(url, user, pass);
+			statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+									ResultSet.CONCUR_READ_ONLY);
+		} catch (SQLException e) {
+			new Alert(e.getMessage(), null);
+		}
+	}
+	
+	public Statement getStatement() {
+		return statement;
+	}
+
+}
